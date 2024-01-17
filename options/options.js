657e3f6d51a54e8c7c6df2277ecb8825ec47291f
brenley-cb1 <brenley+test@crowdbotics.com1>
2024-01-17 01:22:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "demoapp4-4",
+  url: "https://demoapp4-4.botics.co",
+  api: "https://demoapp4-4.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
